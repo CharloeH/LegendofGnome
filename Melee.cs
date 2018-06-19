ef203a15a5bcadb0a2e2f22d31d89f4131e26e30
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2018-06-19 19:39:51 -0400

    Update Melee.cs

--- Melee.cs
+++ Melee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,40 +19,52 @@ namespace LegendofGnome
     class Melee
     {
         private Canvas canvas;
+        private Window window;
         public Rectangle sword = new Rectangle();
         public bool hit;
 
-        public Melee(Canvas c, Window window, Point Point)
+        public Melee(Canvas c, Window w, Point Point)
         {
+            canvas = c;
+            window = w;
             sword.Height = 20;
             sword.Width = 20;
             //BitmapImage bitmapBackground = new BitmapImage(new Uri("Fireball Projectile.png", UriKind.Relative));
             //ImageBrush backgroundBrush = new ImageBrush(bitmapBackground);
             sword.Fill = Brushes.Gray; //backgroundBrush;
             canvas.Children.Add(sword);
-            double direction_x = Mouse.GetPosition(window).X;
-            double direction_y = Mouse.GetPosition(window).Y;
-            if (direction_x <= 0)
+            double direction_x = Mouse.GetPosition(window).X - Point.X;
+            double direction_y = Mouse.GetPosition(window).Y - Point.Y;
+            if (direction_x > 0)
             {
-                Canvas.SetLeft(this.sword, Point.X + 25);
+                Canvas.SetLeft(this.sword, Point.X + 50);
             }
-            Canvas.SetTop(this.sword, Point.Y + 25);
+            if (direction_x < 0)
+            {
+                Canvas.SetLeft(this.sword, Point.X - 20);
+            }
+            if (direction_y > 0)
+            {
+                Canvas.SetTop(this.sword, Point.Y + 50);
+            }
+            if (direction_y < 0)
+            {
+                Canvas.SetTop(this.sword, Point.Y - 20);
+            }
         }
 
-        public bool checkCollision(Point source)
+        public bool checkCollision(Point t, Point s, Rectangle target, Rectangle source)
         {
-            for (int i = 0; i < canvas.Children.Count; i++)
+            bool temp = false;
+
+            if (t.X <= s.X + source.Width & t.Y <= s.Y + source.Height)
             {
-                if (canvas.Children[0].GetType() == typeof(System.Windows.Shapes.Rectangle))
+                if (t.X + target.Width >= s.X & t.Y + target.Height >= s.Y)
                 {
-
-                }
-                if (source.X <= Canvas.GetLeft(canvas.Children[0]) & source.X >= Canvas.GetLeft(canvas.Children[0]) + canvas.Children[0].)
-                {
-
+                    temp = true;
                 }
             }
-            return hit;
+            return temp;
         }
     }
 }
