6fa37015e469e4e859f92be327930ba1e49ffd1e
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2018-06-19 17:42:55 -0400

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -14,59 +14,39 @@ namespace LegendofGnome
 {
     class Player
     {
+      
+      
         
         public Rectangle playerRectangle = new Rectangle();
-         Map map = new Map();
-        public Point Move(Rectangle playerRectangle, Canvas canvas, Point playerPoint, bool isRoom1, bool isRoom2, bool isRoom3)
+        Map map = new Map();
+        public Point Move(Rectangle playerRectangle, Canvas canvas, Point playerPoint, bool isRoom1, bool isRoom2, bool isRoom3, Rectangle door1, Rectangle door2, Rectangle door3, Rectangle wallTop1, Rectangle wallTop2, Rectangle wallLeft1, Rectangle wallLeft2, Rectangle wallRight1, Rectangle wallRight2, Rectangle wallBot1, Rectangle wallBot2)
         {
             if (Keyboard.IsKeyDown(Key.W))
             {
-                if (playerPoint.Y >= 55)
+                if (playerPoint.Y >= 50)
                 {
-                    
+
                     Console.WriteLine("w");
                     playerPoint.Y = playerPoint.Y - 10;
 
                 }
-                if (isRoom1 == true)
-                {
-                    if (playerPoint.Y <= 55 & playerPoint.X >= 450 & playerPoint.X <= 550)
-                    {
-                       // MessageBox.Show("wrong!");
-                        playerPoint.Y -= 10;
-                        if (playerPoint.Y == -50)
-                        {
-                            playerPoint.Y = 1000;
-                        }
-                    }
-                }
+               
+                
             }
             if (Keyboard.IsKeyDown(Key.S))
             {
-                if (playerPoint.Y <= 860)
+                if (playerPoint.Y <= 700)
                 {
-                    Console.WriteLine("s");
+                   
                     playerPoint.Y += 10;
-                    
+
                 }
-               else if (isRoom1 == true)
-                {
-                    if (playerPoint.Y >= 850 & playerPoint.X >= 450 & playerPoint.X <= 550)
-                    {
-                        playerPoint.Y += 10;
-                        {
-                            if (playerPoint.Y == 1050)
-                            {
-                                playerPoint.Y = 0;
-                            }
-                        }
-                    }
-                }
+                
                 
             }
             if (Keyboard.IsKeyDown(Key.A))
             {
-                if (playerPoint.X >= 60)
+                if (playerPoint.X >= 50)
                 {
                     Console.WriteLine("a");
                     playerPoint.X -= 10;
@@ -74,9 +54,9 @@ namespace LegendofGnome
             }
             if (Keyboard.IsKeyDown(Key.D))
             {
-                if (playerPoint.X <= 880)
+                if (playerPoint.X <= 700)
                 {
-                    Console.WriteLine("d");
+                    
                     playerPoint.X += 10;
                 }
             }
@@ -88,7 +68,7 @@ namespace LegendofGnome
         {
             Canvas.SetLeft(playerRectangle, playerPoint.X);
             Canvas.SetTop(playerRectangle, playerPoint.Y);
-           
+
         }
 
         public void GeneratePlayer(Canvas canvas, Point playerPoint)
@@ -101,7 +81,7 @@ namespace LegendofGnome
             canvas.Children.Add(playerRectangle);
             Canvas.SetLeft(playerRectangle, playerPoint.X);
             Canvas.SetTop(playerRectangle, playerPoint.Y);
-            
+
         }
     }
 
